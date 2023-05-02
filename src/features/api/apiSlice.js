f11fe8b56bb3ffdf3b9322481c4a931f5ca69a4d
shz-cod <web.shahidul.alam@gmail.com>
2023-05-03 00:21:00 +0600

    v0.6.2 (Api url setup)

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://127.0.0.1:8000/api/",
+    // baseUrl: "http://127.0.0.1:8000/api/",
+    baseUrl: "https://iits-reg-api.up.railway.app/api/",
   }),
   endpoints: (builder) => ({
     getQuizzes: builder.query({
